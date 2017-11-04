046d0c745d12c2dbc91d4508ec1f456c81130b25
dowgiallo <dowgiallo@gmail.com>
2017-11-04 13:58:36 +0100

    initial commit

---
+++ src/main/java/com/dowg/xa/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.dowg.xa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
